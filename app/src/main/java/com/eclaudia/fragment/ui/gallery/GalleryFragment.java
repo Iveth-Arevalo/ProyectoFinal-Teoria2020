ce86634ad5172286796a682ba4e706e89fdb2f72
Iveth-Arevalo <ivegarev7@gmail.com>
2020-11-30 04:47:57 +0100

    Proyecto teoria final - 2020

---
+++ app/src/main/java/com/eclaudia/fragment/ui/gallery/GalleryFragment.java
@@ -0,0 +1,37 @@
+package com.eclaudia.fragment.ui.gallery;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.eclaudia.fragment.R;
+
+public class GalleryFragment extends Fragment {
+
+    private GalleryViewModel galleryViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        galleryViewModel =
+                ViewModelProviders.of(this).get(GalleryViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+        final TextView textview = root.findViewById(R.id.text_gallery);
+        galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textview.setText(s);
+            }
+        });
+        return root;
+    }
+}
